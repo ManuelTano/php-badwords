7d99bbd551f41798e29dd961ea1acbae20d23a11
ManuelTano <manueltano98@gmail.com>
2022-08-29 18:16:09 +0200

    Added censura e lunghezza. Esercizio completato.

--- index.php
+++ index.php
@@ -6,10 +6,20 @@ Stampiamo di nuovo il paragrafo e la sua nuova lunghezza. -->
 
 <?php
 
-$text= 'Ennò ennò, rischia di rovinare una carriera con un gesto indecente Zinedine Zidane.';
+$text = 'Ennò ennò, rischia di rovinare una carriera con un gesto indecente Zinedine Zidane.';
 
 echo $text;
 echo '<br/> <br/>';
 echo strlen($text);
+echo '<br/> <br/>';
+
+
+$bad_words = $_GET['badwords'];
+$replace_text = str_replace($bad_words, '***', $text);
+
+echo $replace_text;
+echo '<br/> <br/>';
+echo strlen($replace_text);
+
 
 ?>
